f6e3a03988e535ba53098d2432ea1e374e474799
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-18 15:34:00 +0100

    Implemented tests.

--- test/TopTenPops.Tests/CsvReaderTests.cs
+++ test/TopTenPops.Tests/CsvReaderTests.cs
@@ -4,6 +4,12 @@ namespace TopTenPops.Tests
 {
     public class CsvReaderTests
     {
+        private CsvReader GetCsvReader()
+        {
+            string path = "/Users/johnlundgren/Development/netProjects/TopTenPops/src/TopTenPops/countries.csv";
+            return new CsvReader(path);
+        }
+
         [Fact]
         public void CreateCsvReaderObject_Test()
         {
@@ -13,6 +19,19 @@ namespace TopTenPops.Tests
             csvReader = new CsvReader(new string[] { "whatever" });
             //Assert
             Assert.IsType<CsvReader>(csvReader);
+        }
+
+        [Fact]
+        public void ReadFirstZeroCountries_Test()
+        {
+            //Arrange
+            var csvReader = GetCsvReader();
+            int expectedLength = 0;
+            //Act
+            var countries = csvReader.ReadFirstNCountries(0);
+            //Assert
+            Assert.NotNull(countries);
+            Assert.Equal(expectedLength, countries.Length);
         }
     }
 }

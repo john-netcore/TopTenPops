82ec57bbd5a4d7317ec22da67baa0d5732fc9cc6
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-18 10:38:26 +0100

    Added country properties

--- src/TopTenPops/Country.cs
+++ src/TopTenPops/Country.cs
@@ -2,6 +2,16 @@ namespace TopTenPops
 {
     public class Country
     {
-        
+        public int Population { get; set; }
+        public string Region { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public Country(string name, string code, string region, int population)
+        {
+            this.Name = name;
+            this.Code = code;
+            this.Region = region;
+            this.Population = population;
+        }
     }
 }

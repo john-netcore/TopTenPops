32b2ca1b70f5537e0261b64412fea35538a2a336
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-19 10:08:27 +0100

    Changes for using batch iterations displaying countries.

--- src/TopTenPops/Program.cs
+++ src/TopTenPops/Program.cs
@@ -6,14 +6,33 @@ namespace TopTenPops
     {
         static void Main(string[] args)
         {
+            System.Console.Write("Enter number of countries to display: ");
+            int.TryParse(Console.ReadLine(), out int displayNumber);
+            if (displayNumber < 1)
+            {
+                System.Console.WriteLine("You must type a positive int value.");
+                return;
+            }
             string[] path = new string[]{
+                "src",
+                "TopTenPops",
+                "data",
                 "countries.csv"
             };
             var csvReader = new CsvReader(path);
-            var countries = csvReader.ReadFirstNCountries(20);
-            foreach (var country in countries)
+            var countries = csvReader.ReadAllCountries();
+
+            for (var i = 0; i < countries.Count; i++)
             {
-                System.Console.WriteLine($"{country.FormattedPopulation}   {country.Name}");
+                if (i > 0 && (i % displayNumber == 0))
+                {
+                    System.Console.WriteLine("Press enter to continue, any other key to quit");
+                    if (Console.ReadLine() != "")
+                    {
+                        return;
+                    }
+                }
+                System.Console.WriteLine($"{countries[i].Name}:   ${countries[i].Population}");
             }
         }
     }

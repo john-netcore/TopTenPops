2b674ee42af5b6fb89e36e83c7070c7f42d73c77
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-03-22 16:51:35 +0100

    Changed clien to work with regions selections.

--- src/TopTenPops/Program.cs
+++ src/TopTenPops/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace TopTenPops
 {
@@ -6,34 +8,65 @@ namespace TopTenPops
     {
         static void Main(string[] args)
         {
-            System.Console.Write("Enter number of countries to display: ");
-            int.TryParse(Console.ReadLine(), out int displayNumber);
-            if (displayNumber < 1)
-            {
-                System.Console.WriteLine("You must type a positive int value.");
-                return;
-            }
             string[] path = new string[]{
                 "src",
                 "TopTenPops",
                 "data",
                 "countries.csv"
-            };
-            var csvReader = new CsvReader(path);
-            var countries = csvReader.ReadAllCountries();
+                };
 
-            for (var i = 0; i < countries.Count; i++)
+            var csvReader = new CsvReader(path);
+
+            foreach (var region in csvReader.Regions)
             {
-                if (i > 0 && (i % displayNumber == 0))
-                {
-                    System.Console.WriteLine("Press enter to continue, any other key to quit");
-                    if (Console.ReadLine() != "")
-                    {
-                        return;
-                    }
-                }
-                System.Console.WriteLine($"{i + 1} {countries[i].Name}: {countries[i].FormattedPopulation}");
+                System.Console.WriteLine(region);
             }
+
+            System.Console.Write("Enter a region from above for countries to display: ");
+            string chosenRegion = System.Console.ReadLine();
+            chosenRegion = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(chosenRegion);
+
+            try
+            {
+                var countries = csvReader.GetCountries(chosenRegion);
+
+                foreach (var country in countries)
+                {
+                    System.Console.WriteLine($"{country.Name}: {country.FormattedPopulation}");
+                }
+            }
+            catch (KeyNotFoundException)
+            {
+                System.Console.WriteLine("Chosen region does not exist");
+            }
+            // System.Console.Write("Enter number of countries to display: ");
+            // int.TryParse(Console.ReadLine(), out int displayNumber);
+            // if (displayNumber < 1)
+            // {
+            //     System.Console.WriteLine("You must type a positive int value.");
+            //     return;
+            // }
+            // string[] path = new string[]{
+            //     "src",
+            //     "TopTenPops",
+            //     "data",
+            //     "countries.csv"
+            // };
+            // var csvReader = new CsvReader(path);
+            // var countries = csvReader.ReadAllCountries();
+
+            // for (var i = 0; i < countries.Count; i++)
+            // {
+            //     if (i > 0 && (i % displayNumber == 0))
+            //     {
+            //         System.Console.WriteLine("Press enter to continue, any other key to quit");
+            //         if (Console.ReadLine() != "")
+            //         {
+            //             return;
+            //         }
+            //     }
+            //     System.Console.WriteLine($"{i + 1} {countries[i].Name}: {countries[i].FormattedPopulation}");
+            // }
         }
     }
 }
